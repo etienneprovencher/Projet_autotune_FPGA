d243b4bd96573de4d0eea83c5edad483a083af8f
Etienne Provencher <proe0702@usherbrooke.ca>
2024-03-27 16:34:14 -0400

    M1

--- Atelier_FFT/Vitis_projet3/design_1_wrapper/platform.tcl
+++ Atelier_FFT/Vitis_projet3/design_1_wrapper/platform.tcl
@@ -40,3 +40,6 @@ platform generate
 platform generate
 platform generate
 platform generate -domains standalone_microblaze_0,zynq_fsbl 
+platform generate -domains standalone_microblaze_0,zynq_fsbl 
+platform clean
+platform generate
